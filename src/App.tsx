0c69e098a0298b1766611f082b090f186d41a21d
pavelminsk1979 <pavelminsk1979@mail.ru>
2022-09-16 19:51:33 +0300

    frame Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Counter} from "./Counter";
 import {Settings} from "./Settings";
 import {HashRouter, NavLink, Routes,Route} from "react-router-dom";
@@ -6,12 +6,19 @@ import {HashRouter, NavLink, Routes,Route} from "react-router-dom";
 
 function App() {
 
-    const addedPlusOneHandler = () => {
+    const starnNumber=0
+    const finishNumber=5
 
+    const[number,SetNumber]=useState(starnNumber)
+
+    const addedPlusOneHandler = () => {
+if(number<finishNumber){
+    SetNumber(number+1)
+}
     }
 
     const setZeroHandler = () => {
-
+        SetNumber(starnNumber)
     }
 
     const setingsHandler = () => {
@@ -23,11 +30,13 @@ function App() {
         <div>
             <HashRouter>
                 <NavLink to={'/counter'}><h2>counter</h2></NavLink>
-                <NavLink to={'/setting'}><h2>setting</h2></NavLink>
+                {/*<NavLink to={'/setting'}><h2>setting</h2></NavLink>*/}
 
                 <Routes>
                     <Route path={'/counter'}
                            element={<Counter
+                               finishNumber={finishNumber}
+                               number={number}
                                addedPlusOneHandler={addedPlusOneHandler}
                                setZeroHandler={setZeroHandler}
                                setingsHandler={setingsHandler}
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import st from './Counter.module.css';
 import {Button} from "./Button";
+import {NavLink} from "react-router-dom";
 
 type CounterType = {
     addedPlusOneHandler: () => void
     setZeroHandler: () => void
     setingsHandler: () => void
+    number: number
+    finishNumber: number
 }
 
-export function Counter(props: CounterType) {
+export function Counter({
+                            addedPlusOneHandler,
+                            setZeroHandler,
+                            setingsHandler,
+                            number,
+                            finishNumber,
+                        }: CounterType) {
 
 
     return (
         <div className={st.frame}>
-            <div>0</div>
+            <div className={
+                number==finishNumber?st.finishNumber:st.number}>{number}</div>
             <div className={st.buttonGroup}>
                 <Button
                     name={'plus and again plus and again plus 1'}
-                    callback={props.addedPlusOneHandler} className={st.plus1}/>
+                    callback={addedPlusOneHandler} className={st.plus1}/>
                 <Button
                     name={'start over'}
-                    callback={props.setZeroHandler}
+                    callback={setZeroHandler}
                     className={st.start}/>
-                <Button
+                <NavLink to={'/setting'}> <Button
                     name={'settings'}
-                    callback={props.setingsHandler}
-                    className={st.setting}/>
+                    callback={setingsHandler}
+                    className={st.setting}/></NavLink>
 
             </div>
         </div>
